a6f5a482e0b85dce8abc80a7c2d524319dc27fa6
Patryk Rejent <82150867+Patryk244@users.noreply.github.com>
2026-01-12 18:48:31 +0100

    add a view for perosn and possible to add new person

--- src/main/java/com/project/first_vaddin/views/PersonView.java
+++ src/main/java/com/project/first_vaddin/views/PersonView.java
@@ -1,23 +1,23 @@
 package com.project.first_vaddin.views;
 
 import com.project.first_vaddin.domain.Person;
-import com.project.first_vaddin.repository.PersonRepository;
 import com.project.first_vaddin.service.PersonService;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@Route("")
+@Route("/persons")
 @PageTitle("Mange people")
 public class PersonView extends VerticalLayout {
-     PersonView(PersonRepository personService) {
+     PersonView(PersonService personService) {
        var grid = new Grid<Person>();
        grid.addColumn(Person::getFirstName).setHeader("First Name");
        grid.addColumn(Person::getLastName).setHeader("Last Name");
+       grid.addColumn(Person::getEmail).setHeader("Email");
 
        grid.setItemsPageable(pageable -> personService
-               .findAll(pageable)
+               .getAllPersons(pageable)
                .getContent()
        );
        setSizeFull();
